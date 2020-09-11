c70cb0ff522810815e1a272ed04fe31287392c3e
Your Name <artemnesterov1991@gmail.com>
2020-09-11 20:57:30 +0300

    add def

--- game.py
+++ game.py
@@ -1,10 +1,67 @@
-import exceptions
+from exceptions import GameOver, EnemyDown
+import model
+from settings import WELCOME_STRING, START_STRING, START_EXIT_KEY_WORDS, GOODBYE_STRING, WRONG_SELECT, LIVES, V_SCORE, \
+    ENEMY_DOWN_STRING, LIVES_STRING
+
+
+def input_player_name(string=WELCOME_STRING):
+    return input(string)
+
+
+def input_start(string=START_STRING):
+    i = ''
+    while i.upper() not in [x.upper() for x in START_EXIT_KEY_WORDS.values()]:
+        i = input(string).strip()
+    if i == START_EXIT_KEY_WORDS['exit']:
+        print(GOODBYE_STRING)
+        exit()
+
+
+def select_player_attack(string="введи 1 длявыбора волшебника, 2 для воина, 3 для разбойника"):
+    while True:
+        i = input(string)
+        if i.isdigit():
+            i = int(i)
+        if i in [1, 2, 3]:
+            return i
+        print(WRONG_SELECT)
+
+
+def play():
+    """
+    - Ввод имени игрока
+    - Создание объекта player
+    - level = 1
+    - Создание объекта enemy
+    - в бесконечном цикле вызывает методы attack и defense объекта player
+    - при возникновении исключения EnemyDown повышает уровень игры, создает новый объект Enemy с
+    новым уровнем, добавляет игроку +5 очков."""
+
+    level = 1
+    player = model.Player(input_player_name(), LIVES)
+    player.input_start()
+    enemy = model.Enemy(level)
+    while True:
+        try:
+            player.attack(enemy)
+            player.defence(enemy)
+        except EnemyDown:
+            level += 1
+            enemy = model.Enemy(level)
+            player.score += V_SCORE
+            print(ENEMY_DOWN_STRING, player.score)
+            print(LIVES_STRING, player.lives)
+
+            continue
+        except GameOver:  # запись очков игрока
+            with open("scores.txt", "a") as f:
+                f.writelines("{}:  {}".format(player.name, player.score))
+
 
 if __name__ == '__main__':
     try:
-        pass
-
-    except exceptions.GameOver:
+        play()
+    except GameOver:
         # выводит
         # сообщение
         # об
@@ -20,27 +77,3 @@ if __name__ == '__main__':
 
     finally:
         print("Good bye!")
-
-
-        # - Содержит блок на проверку имени модуля (main)
-        #     - внутри if блок try/except.
-        #     - try запускает функцию play()
-        #     - except обрабатывает два исключения:
-        #         GameOver - выводит сообщение об ошибке, записывает результат в таблицу рекордов.
-        #         KeyboardInterrupt - pass
-        #     - finally печатает "Good bye!"
-
-
-def play():
-    name = input()
-
-
-
-        # - Ввод имени игрока
-        #     - Создание объекта player
-        #     - level = 1
-        #     - Создание объекта enemy
-        #     - в бесконечном цикле вызывает методы attack и defense объекта player
-        #     - при возникновении исключения EnemyDown повышает уровень игры, создает новый объект Enemy с
-        #     новым уровнем, добавляет игроку +5 очков.
-    pass
--- model.py
+++ model.py
@@ -1,119 +1,90 @@
-class Player:
-    def __init__(self, name, lives, score, allowed_attacks):
-        self.name = name
-        self.lives = lives
-        self.score = score
-        self.allowed_attacks = allowed_attacks
-
-    @staticmethod
-    def fight(attack, defense):
-        # статический
-        # fight(attack, defense) - возвращает
-        # результат
-        # раунда - 0
-        # если
-        # ничья, -1
-        # если
-        # атака
-        # неуспешна, 1
-        # если
-        # атака
-        # успешна.
-        pass
-
-    def decrease_lives(self):
-        # то
-        # же, что
-        # и
-        # Enemy.decrease_lives(), вызывает
-        # исключение
-        # GameOver
-        pass
-
-    def attack(self, enemy_obj):
-        # получает
-        # ввод
-        # от
-        # пользователя(1, 2, 3), выбирает
-        # атаку
-        # противника
-        # из
-        # объекта
-        # enemy_obj;
-        # вызывает
-        # метод
-        # fight();
-        # Если
-        # результат
-        # боя
-        # 0 - вывести
-        # "It's a draw!",
-        # если
-        # 1 = "You attacked successfully!"
-        # и
-        # уменьшает
-        # количество
-        # жизней
-        # противника
-        # на
-        # 1, если - 1 = "You missed!"
-        pass
-
-    def defence(self, enemy_obj):
-        # то
-        # же
-        # самое, что
-        # и
-        # метод
-        # attack(), только
-        # в
-        # метод
-        # fight
-        # первым
-        # передается
-        # атака
-        # противника, и
-        # при
-        # удачной
-        # атаке
-        # противника
-        # вызывается
-        # метод
-        # decrease_lives
-        # игрока.
-
-        pass
+from random import randint
+from exceptions import EnemyDown, GameOver
+from game import select_player_attack
+from settings import ATTACK_STRING, DEFENSE_STRING, GAME_OVER_STRING, LIVES_STRING
 
 
 class Enemy:
-    def __init__(self, level, lives):
+    """- свойства - level, lives.
+    - конструктор принимает уровень. Уровень жизней противнка = уровень противника."""
+
+    def __init__(self, level):
         self.level = level
+        self.lives = level
+
+    @staticmethod
+    def select_attack():
+        """рандомно выбирает атаку у пративника"""
+        return randint(1, 3)
+
+    def decrease_lives(self):
+        """уменьшает количество жизней. Когда жизней становится 0 вызывает
+        исключение EnemyDown."""
+        self.lives -= 1
+        if not self.lives:
+            raise EnemyDown
+
+
+class Player:
+    score = 0
+    allowed_attacks = [1, 2, 3]
+
+    def __init__(self, name, lives):
+        self.name = name
         self.lives = lives
+        pass
 
     @staticmethod
     def fight(attack, defense):
-        # возвращает
-        # результат
-        # раунда - 0
-        # если
-        # ничья, -1
-        # если
-        # атака
-        # неуспешна, 1
-        # если
-        # атака
-        # успешна.
-        pass
+
+        """- возвращает результат раунда -
+        0 если ничья,
+        -1 если атака неуспешна,
+        1 если атака успешна.
+        """
+        # draw
+        if attack == defense:
+            return 0
+        # lose
+        if attack == 1 and defense == 2 or \
+                                attack == 2 and defense == 3 or \
+                                attack == 3 and defense == 1:
+            return -1
+        # won
+        if attack == 2 and defense == 1 or \
+                                attack == 3 and defense == 2 or \
+                                attack == 1 and defense == 3:
+            return 1
 
     def decrease_lives(self):
-        # уменьшает
-        # количество
-        # жизней.Когда
-        # жизней
-        # становится
-        # 0
-        # вызывает
-        # исключение
-        # EnemyDown.
+        """- то  же, что и Enemy.decrease_lives(), вызывает исключение
+            GameOver."""
+        self.lives -= 1
+        if not self.lives:
+            print(GAME_OVER_STRING, self.score)
+            print(LIVES_STRING, self.lives)
+            raise GameOver
 
-        pass
+    def attack(self, enemy_obj):
+        """- получает ввод от пользователя(1, 2, 3), выбирает
+            атаку противника из объекта enemy_obj;
+             вызывает метод fight();
+            Если результат боя 0 - вывести "It's a draw!",
+            если 1 = "You attacked successfully!" и уменьшает количество жизней противника на 1,
+            если - 1 = "You missed!"""
+        my_choice = select_player_attack()
+        result = self.fight(my_choice, enemy_obj.select_attack())
+        print(ATTACK_STRING[result])
+        if result == 1:
+            enemy_obj.decrease_lives()
+            self.score += 1
+
+    def defence(self, enemy_obj):
+        """- то же самое, что и метод attack(),
+        только в метод fight первым передается атака противника,
+        и при удачной атаке противника вызывается метод decrease_lives игрока."""
+        my_choice = select_player_attack()
+        result = self.fight(enemy_obj.select_attack(), my_choice)
+        print(DEFENSE_STRING[result])
+        if result == 1:
+            self.decrease_lives()
